84d4d61b64e0b5b028c87fe8b603912a08c29e4a
Rina Mito <satoshikibu@gmail.com>
2019-03-02 23:58:44 +0900

    KATANA_TYPEを追加

--- app/models/katana.rb
+++ app/models/katana.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class Katana < ApplicationRecord
+  KATANA_TYPE = {
+    1 => '短刀',
+    2 => '脇差',
+    3 => '打刀',
+    4 => '太刀',
+    5 => '大太刀',
+    6 => '槍',
+    7 => '薙刀',
+    8 => '剣'   
+  }
 end
