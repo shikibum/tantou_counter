f69ddfe85647e48e85c2ea490ed51604fe3e1e9b
Rina Mito <satoshikibu@gmail.com>
2019-03-08 22:41:29 +0900

    レシピごと結果を1つのハッシュで返すようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,30 +5,31 @@ class UsersController < ApplicationController
     user = User.find_by(share_id: params[:id])
     @tantous = user&.tantous.where.not(katana_id: nil).order(created_at: :desc)
     res = user.tantous.joins(:katana).group('katanas.katana_type').count
-    @rates = res.map{ |id, v| [id, v.to_f / res.values.sum] }.to_h
+    @rates = res.map { |id, v| [id, v.to_f / res.values.sum] }.to_h
     @rares = user.tantous.joins(:katana).group('katanas.rare').count
     @rares_with_fuda = user.tantous.joins(:katana).group('katanas.rare').where(fuda: %i[梅 竹 松 富士]).count
     @rares_without_fuda = user.tantous.joins(:katana).group('katanas.rare').where(fuda: %i[なし]).count
 
-    res_by_recipe = user.tantous.joins(:katana).group('mokutan', 'tamahagane', 'reikyakuzai', 'toishi').order('mokutan', 'tamahagane', 'reikyakuzai', 'toishi').count
-    rares_by_recipe = user.tantous.joins(:katana).group('mokutan', 'tamahagane', 'reikyakuzai', 'toishi').order('mokutan', 'tamahagane', 'reikyakuzai', 'toishi').where(katanas: { rare: true }).count
-    @tatal_by_recipe = res_by_recipe.map { |recipe, count|
-                        rare_count = rares_by_recipe[recipe]
-                        [recipe, { rare: rare_count, total: count, rate: rare_count.to_f/count}]
-                       }.to_h
-
+    # {[50, 50] => { rare: 3, total: 10, rate: 0.33333 } }
     res_by_recipe_with_fuda = user.tantous.joins(:katana).group('mokutan', 'tamahagane', 'reikyakuzai', 'toishi').order('mokutan', 'tamahagane', 'reikyakuzai', 'toishi').where(tantous: { fuda: %i[梅 竹 松 富士] }).count
     rares_by_recipe_with_fuda = user.tantous.joins(:katana).group('mokutan', 'tamahagane', 'reikyakuzai', 'toishi').order('mokutan', 'tamahagane', 'reikyakuzai', 'toishi').where(katanas: { rare: true }, tantous: { fuda: %i[梅 竹 松 富士] }).count
-    @total_by_recipe_with_fuda = res_by_recipe_with_fuda.map { |recipe, count|
-                                   rare_count = rares_by_recipe_with_fuda[recipe]
-                                   [recipe, { rare: rare_count, total: count, rate: rare_count.to_f/count}]
-                                 }.to_h
+    @total_by_recipe_with_fuda = res_by_recipe_with_fuda.map do |recipe, count|
+      rare_count = rares_by_recipe_with_fuda[recipe]
+      [recipe, { rare: rare_count.to_i, total: count, rate: rare_count.to_f / count }]
+    end.to_h
 
-    res_by_recipe_without_fuda = user.tantous.joins(:katana).group('mokutan', 'tamahagane', 'reikyakuzai', 'toishi').order('mokutan', 'tamahagane', 'reikyakuzai', 'toishi').where(tantous: { fuda: %i[なし] }).count                             
+    res_by_recipe_without_fuda = user.tantous.joins(:katana).group('mokutan', 'tamahagane', 'reikyakuzai', 'toishi').order('mokutan', 'tamahagane', 'reikyakuzai', 'toishi').where(tantous: { fuda: %i[なし] }).count
     rares_by_recipe_without_fuda = user.tantous.joins(:katana).group('mokutan', 'tamahagane', 'reikyakuzai', 'toishi').order('mokutan', 'tamahagane', 'reikyakuzai', 'toishi').where(katanas: { rare: true }, tantous: { fuda: %i[なし] }).count
-    @total_by_recipe_without_fuda = res_by_recipe_without_fuda.map { |recipe, count|
-                                   rare_count = rares_by_recipe_without_fuda[recipe]
-                                   [recipe, { rare: rare_count, total: count, rate: rare_count.to_f/count}]
-                                 }.to_h
+    @total_by_recipe_without_fuda = res_by_recipe_without_fuda.map do |recipe, count|
+      rare_count = rares_by_recipe_without_fuda[recipe]
+      [recipe, { rare: rare_count.to_i, total: count, rate: rare_count.to_f / count }]
+    end.to_h
+    # {[50, 50] => {fuda: { rare: 3, total: 10, rate: 0.33333 }, without_fuda: { rare: 3, total: 10, rate: 0.33333 }}
+    recipes = (@total_by_recipe_with_fuda.keys + @total_by_recipe_without_fuda.keys).uniq
+    @total_by_recipe = recipes.map do |recipe|
+      with_fuda = @total_by_recipe_with_fuda[recipe]
+      without_fuda = @total_by_recipe_without_fuda[recipe]
+      [recipe, { with_fuda: with_fuda, without_fuda: without_fuda }]
+    end.to_h
   end
 end
