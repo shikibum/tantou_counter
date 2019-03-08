ca95962207ca3740ba70d00064e8b8f2d2c33232
Rina Mito <satoshikibu@gmail.com>
2019-03-08 21:42:03 +0900

    グラフの追加

---
+++ app/assets/javascripts/graph.js
@@ -0,0 +1,41 @@
+$(function () {
+  const chartData = $('.chart-data').data('chart');
+  $('#container').highcharts({
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: '刀種結果'
+    },
+    xAxis: {
+      categories: ['刀種']
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: ''
+      }
+    },
+    legend: {
+      reversed: true
+    },
+    plotOptions: {
+      series: {
+        stacking: 'percent',
+        dataLabels: {
+          enabled: true,
+          formatter: function () {
+            return this.percentage.toFixed(1) + '%';
+          }
+        },
+      }
+    },
+    credits: {
+      enabled: false
+    },
+    exporting: {
+      enabled: false
+    },
+    series: chartData
+  });
+});
