fac21bc6d66d69e3785c8a028afd267e6e4942c5
Rina Mito <satoshikibu@gmail.com>
2019-05-25 19:03:35 +0900

    private methodに追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,25 +10,36 @@ class UsersController < ApplicationController
     @rares_without_fuda = user.tantous.rare_katana.without_fuda.count
 
     # {[50, 50] => { rare: 3, total: 10, rate: 0.33333 } }
-    res_by_recipe_with_fuda = user.tantous.group_by_shizai.order_by_shizai.with_fuda.count
-    rares_by_recipe_with_fuda = user.tantous.rare_katana.group_by_shizai.order_by_shizai.with_fuda.count
-    @total_by_recipe_with_fuda = res_by_recipe_with_fuda.map do |recipe, count|
-      rare_count = rares_by_recipe_with_fuda[recipe]
-      [recipe, { rare: rare_count.to_i, total: count, rate: rare_count.to_f / count }]
-    end.to_h
+    @total_by_recipe_with_fuda = retrieve_total_by_recipe_with_fuda(user)
 
-    res_by_recipe_without_fuda = user.tantous.group_by_shizai.order_by_shizai.without_fuda.count
-    rares_by_recipe_without_fuda = user.tantous.rare_katana.group_by_shizai.order_by_shizai.without_fuda.count
-    @total_by_recipe_without_fuda = res_by_recipe_without_fuda.map do |recipe, count|
-      rare_count = rares_by_recipe_without_fuda[recipe]
-      [recipe, { rare: rare_count.to_i, total: count, rate: rare_count.to_f / count }]
-    end.to_h
+    @total_by_recipe_without_fuda = retrieve_total_by_recipe_without_fuda(user)
+
     # {[50, 50] => {fuda: { rare: 3, total: 10, rate: 0.33333 }, without_fuda: { rare: 3, total: 10, rate: 0.33333 }}
     recipes = (@total_by_recipe_with_fuda.keys + @total_by_recipe_without_fuda.keys).uniq
     @total_by_recipe = recipes.map do |recipe|
       with_fuda = @total_by_recipe_with_fuda[recipe]
       without_fuda = @total_by_recipe_without_fuda[recipe]
       [recipe, { with_fuda: with_fuda, without_fuda: without_fuda }]
+    end.to_h
+  end
+
+  private
+
+  def retrieve_total_by_recipe_with_fuda(user)
+    res_by_recipe_with_fuda = user.tantous.group_by_shizai.order_by_shizai.with_fuda.count
+    rares_by_recipe_with_fuda = user.tantous.rare_katana.group_by_shizai.order_by_shizai.with_fuda.count
+    res_by_recipe_with_fuda.map do |recipe, count|
+      rare_count = rares_by_recipe_with_fuda[recipe]
+      [recipe, { rare: rare_count.to_i, total: count, rate: rare_count.to_f / count }]
+    end.to_h
+  end
+
+  def retrieve_total_by_recipe_without_fuda(user)
+    res_by_recipe_without_fuda = user.tantous.group_by_shizai.order_by_shizai.without_fuda.count
+    rares_by_recipe_without_fuda = user.tantous.rare_katana.group_by_shizai.order_by_shizai.without_fuda.count
+    res_by_recipe_without_fuda.map do |recipe, count|
+      rare_count = rares_by_recipe_without_fuda[recipe]
+      [recipe, { rare: rare_count.to_i, total: count, rate: rare_count.to_f / count }]
     end.to_h
   end
 end
